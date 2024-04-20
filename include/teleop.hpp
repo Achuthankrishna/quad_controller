780c9c04f42d471324acb1265150d3fb7e73be6a
Achuthan Krishna <achkrish07@gmail.com>
2024-04-20 01:43:28 -0400

    created as teleop.hpp

---
+++ include/teleop.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file teleop.hpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2024-04-20
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/bool.hpp>
+
+class TeleopTwistKeyboard : public rclcpp::Node {
+public:
+    TeleopTwistKeyboard();
+    ~TeleopTwistKeyboard();
+
+private:
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
+    rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr arm_pub_;
+
+    void publishTwist();
+    void armToggleCallback(const std_msgs::msg::Bool::SharedPtr msg);
+};
